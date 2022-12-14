b6dc35f6cb50fbff34ec125bc61fb1793ff334df
Iryna <ira.khomenko23@gmail.com>
2022-12-14 21:12:25 +0200

    first commit

---
+++ dz2.py
@@ -0,0 +1,11 @@
+a = int(input('Enter N number:'))
+if a % 2 == 1 and a > 20:
+    print('Not Weird')
+elif a % 2 == 0 and 2 <= a <= 5:
+    print('Not Weird')
+elif a % 2 == 0 and 6 <= a <= 20:
+    print('Weird')
+elif a % 2 == 1:
+    print('Weird')
+else:
+    print('Data is invalid')
