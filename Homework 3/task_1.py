0efbac05c16aaf11cf1508c0ad814b10d10e19d1
Iryna <ira.khomenko23@gmail.com>
2022-12-14 22:59:08 +0200

    Homeworks

---
+++ Homework 3/task_1.py
@@ -0,0 +1,14 @@
+""""
+Пользователь вводит с клавиатуры три числа в переменные a, b, c. 
+Если все числа больше 10 и первые два числа делятся на 3, то вывести yes, иначе no
+
+"""""
+
+a = int(input('Please enter A number:'))
+b = int(input('Please enter B number:'))
+c = int(input('Please enter C number:'))
+if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
+    print('yes')
+else:
+    print('no')
+
---
+++ Homework 3/task_2.py
@@ -0,0 +1,8 @@
+"""""
+# Пользователь вводит с клавиатуры три числа в переменные a, b, c.
+# Найдите наибольшее число из них и запишите в переменную max.
+"""""
+a = int(input('Please enter A number:'))
+b = int(input('Please enter B number:'))
+c = int(input('Please enter C number:'))
+print(f'max =  {max(a, b, c)}')
