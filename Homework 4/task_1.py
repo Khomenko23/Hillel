7ee2ce6186083a7170903acdf67850229db14c0e
Iryna <ira.khomenko23@gmail.com>
2023-01-02 20:34:19 +0200

    Homework 12/12

--- Homework 4/task_1.py
+++ Homework 4/task_1.py
@@ -3,7 +3,6 @@
 Если все числа больше 10 и первые два числа делятся на 3, то вывести yes, иначе no
 
 """""
-
 a = int(input('Please enter A number:'))
 b = int(input('Please enter B number:'))
 c = int(input('Please enter C number:'))
@@ -11,4 +10,3 @@ if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
     print('yes')
 else:
     print('no')
-
