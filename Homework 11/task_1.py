52c1aeb7436fcbe01684610a952b2fde017a8dcd
Iryna <ira.khomenko23@gmail.com>
2023-01-25 19:16:12 +0200

    Homework 23/01

---
+++ Homework 11/task_1.py
@@ -0,0 +1,6 @@
+A = {'Alex', 'Maria', 'Ann', 'Iryna', 'Olena'}
+B = {'Ann', 'Olena', 'Mark', 'Ivan', 'Viktoriia'}
+all_debtors = A.union(B)
+oct_debtors = B.difference(A)
+
+print(f'The debtors for both months: {all_debtors};\nthe debtors for October: {oct_debtors}')
